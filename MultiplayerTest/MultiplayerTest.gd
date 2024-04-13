9d0411a43e8fb425c9b77e363f66d52c9b45d1a7
JordanLevy <jordaneblevy@gmail.com>
2024-04-13 01:10:40 -0400

    Added UPNP

--- MultiplayerTest/MultiplayerTest.gd
+++ MultiplayerTest/MultiplayerTest.gd
@@ -1,21 +1,69 @@
-extends Node2D
+extends Node
 
+@onready var main_menu = $CanvasLayer/MainMenu
+@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+@onready var hud = $CanvasLayer/HUD
+@onready var health_bar = $CanvasLayer/HUD/HealthBar
+
+
+const PORT = 9999
 var peer = ENetMultiplayerPeer.new()
 @export var player_scene: PackedScene
 
+func _unhandled_input(event):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().quit()
 
 func _on_host_pressed():
-	peer.create_server(135)
+	main_menu.hide()
+	hud.show()
+	peer.create_server(PORT)
 	multiplayer.multiplayer_peer = peer
-	multiplayer.peer_connected.connect(_add_player)
-	_add_player()
-	
-func _add_player(id = 1):
-	var player = player_scene.instantiate()
-	player.name = str(id)
-	call_deferred("add_child", player)
-
+	multiplayer.peer_connected.connect(add_player)
+	multiplayer.peer_disconnected.connect(remove_player)
+	add_player(multiplayer.get_unique_id())
+	upnp_setup()
 
 func _on_join_pressed():
-	peer.create_client("localhost", 135)
+	main_menu.hide()
+	hud.show()
+	peer.create_client(address_entry.text, PORT)
 	multiplayer.multiplayer_peer = peer
+
+func add_player(peer_id):
+	var player = player_scene.instantiate()
+	player.name = str(peer_id)
+	add_child(player)
+	if player.is_multiplayer_authority():
+		print("connect")
+		player.health_changed.connect(update_health_bar)
+		player.health_changed.emit(player.health)
+
+func remove_player(peer_id):
+	var player = get_node_or_null(str(peer_id))
+	if player:
+		player.queue_free()
+
+func update_health_bar(health_value):
+	health_bar.value = health_value
+	print(health_value)
+
+func _on_multiplayer_spawner_spawned(node):
+	if node.is_multiplayer_authority():
+		node.health_changed.connect(update_health_bar)
+
+func upnp_setup():
+	var upnp = UPNP.new()
+	
+	var discover_result = upnp.discover()
+	assert(discover_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Discover Failed! Error %s" % discover_result)
+
+	assert(upnp.get_gateway() and upnp.get_gateway().is_valid_gateway(), \
+		"UPNP Invalid Gateway!")
+
+	var map_result = upnp.add_port_mapping(PORT)
+	assert(map_result == UPNP.UPNP_RESULT_SUCCESS, \
+		"UPNP Port Mapping Failed! Error %s" % map_result)
+	
+	print("Success! Join Address: %s" % upnp.query_external_address())
--- Player/Player.gd
+++ Player/Player.gd
@@ -1,11 +1,62 @@
 extends CharacterBody2D
 
-var speed = 400.0
+signal health_changed(health_value)
+
+@onready var anim_player = $AnimationPlayer
+
+var health = 3
+const SPEED = 100.0
+
+var mouse_position: Vector2
 
 func _enter_tree():
-	set_multiplayer_authority(name.to_int())
+	set_multiplayer_authority(str(name).to_int())
+
+func _ready():
+	if not is_multiplayer_authority(): return
+
+func _unhandled_input(event):
+	if not is_multiplayer_authority(): return
+	
+	if event is InputEventMouseMotion:
+		mouse_position = event.position
+	
+	if Input.is_action_just_pressed("melee") \
+			and anim_player.current_animation != "melee":
+		play_melee_effects.rpc()
+		#hit_player.receive_damage.rpc_id(hit_player.get_multiplayer_authority())
 
 func _physics_process(delta):
-	if is_multiplayer_authority():
-		velocity = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down") * speed
+	if not is_multiplayer_authority(): return
+
+	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	var direction = input_dir.normalized()
+	velocity = direction * SPEED
+	rotation = atan2(mouse_position.y - position.y, mouse_position.x - position.x)
+
+	if anim_player.current_animation == "melee":
+		pass
+	elif input_dir != Vector2.ZERO and is_on_floor():
+		anim_player.play("move")
+	else:
+		anim_player.play("idle")
+
 	move_and_slide()
+
+@rpc("call_local")
+func play_melee_effects():
+	anim_player.stop()
+	anim_player.play("melee")
+	print("melee")
+
+@rpc("any_peer")
+func receive_damage():
+	health -= 1
+	if health <= 0:
+		health = 3
+		position = Vector2.ZERO
+	health_changed.emit(health)
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "melee":
+		anim_player.play("idle")
