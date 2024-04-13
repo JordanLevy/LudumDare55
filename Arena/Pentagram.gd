c56a906fb5270c9904c99ff904c3feabfd1bce98
JordanLevy <jordaneblevy@gmail.com>
2024-04-13 10:51:26 -0400

    Created candle particles

--- Arena/Pentagram.gd
+++ Arena/Pentagram.gd
@@ -1,6 +1,10 @@
 extends Line2D
 
-@export var radius = 300
+@export var radius = 280
+@export var candle_scene: PackedScene
+
+var candles: Array
+var candle_offset = Vector2(0, -10)
 
 func _ready():
 	var offset = get_viewport_rect().size / 2.0
@@ -8,6 +12,10 @@ func _ready():
 	for i in [5*PI/10, 13*PI/10, PI/10, 9*PI/10, 17*PI/10, 5*PI/10]:
 		var point = Vector2(cos(i), -sin(i)) * radius + offset
 		add_point(point)
+		var candle = candle_scene.instantiate()
+		candle.position = point + candle_offset
+		add_child(candle)
+		candles.append(candle)
 		
 	var num_circle_points = 30
 	for i in range(num_circle_points + 1):
