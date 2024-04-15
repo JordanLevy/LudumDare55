64a3d2d7f21b5c3a1fa329cf9b0d9f706330f6cc
JordanLevy <jordaneblevy@gmail.com>
2024-04-14 23:12:53 -0400

    Title screen

--- MultiplayerTest/MultiplayerGame.gd
+++ MultiplayerTest/MultiplayerGame.gd
@@ -1,7 +1,7 @@
 extends Node
 
 @onready var main_menu = $CanvasLayer/MainMenu
-@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/AddressEntry
+@onready var address_entry = $CanvasLayer/MainMenu/MarginContainer/VBoxContainer/HBoxContainer/AddressEntry
 @onready var hud = $CanvasLayer/HUD
 @onready var health_bar = $CanvasLayer/HUD/HealthBar
 
